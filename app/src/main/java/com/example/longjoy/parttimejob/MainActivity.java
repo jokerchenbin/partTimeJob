fa2197d5934cb6ec0c84c285f44cf3fab14d1737
jokerchenbin <771214723@qq.com>
2015-12-22 23:24:59 +0800

    ....dafadf

--- app/src/main/java/com/example/longjoy/parttimejob/MainActivity.java
+++ app/src/main/java/com/example/longjoy/parttimejob/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     private String name;
     private String password;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
