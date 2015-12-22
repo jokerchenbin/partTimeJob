597542eaba6128dfc0fc10c0a58bd6e76f3b28e7
jokerchenbin <771214723@qq.com>
2015-12-22 23:25:45 +0800

    tanghaibo

--- app/src/main/java/com/example/longjoy/parttimejob/MainActivity.java
+++ app/src/main/java/com/example/longjoy/parttimejob/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private String name;
-    private String password;
+    private String password = "tanghaibo";
 
 
     @Override
