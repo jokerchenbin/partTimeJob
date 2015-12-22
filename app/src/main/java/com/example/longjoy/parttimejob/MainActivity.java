d2a3f94f0df411cd6a2e1b74b0e8d7cc8f135c5d
jokerchenbin <771214723@qq.com>
2015-12-22 23:17:55 +0800

    ....dafadf

--- app/src/main/java/com/example/longjoy/parttimejob/MainActivity.java
+++ app/src/main/java/com/example/longjoy/parttimejob/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private String name;
-
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
