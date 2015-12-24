98be23c7c3597fe653359562a6c6e9a5a841c983
bin_chen <bin_chen@loongjoy.com>
2015-12-24 16:15:32 +0800

    dd

--- app/src/main/java/com/example/longjoy/parttimejob/MainActivity.java
+++ app/src/main/java/com/example/longjoy/parttimejob/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     private String name;
     private String password = "tanghaibo";
     private String chenbin;
-
+    private String bin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
