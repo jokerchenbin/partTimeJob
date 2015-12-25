ffb4d87df082eec0e28a856296877cb621194eb1
bin_chen <bin_chen@loongjoy.com>
2015-12-25 11:34:33 +0800

    注册页面

--- app/src/main/java/com/example/longjoy/parttimejob/activity/LoginActivity.java
+++ app/src/main/java/com/example/longjoy/parttimejob/activity/LoginActivity.java
@@ -1,15 +1,24 @@
 package com.example.longjoy.parttimejob.activity;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.longjoy.parttimejob.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView forgetPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initViewID();
+    }
+
+    private void initViewID() {
+        forgetPass = (TextView)findViewById(R.id.activity_login_tv_forgetPass);
+        forgetPass.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
     }
 }
---
+++ app/src/main/java/com/example/longjoy/parttimejob/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.longjoy.parttimejob.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.longjoy.parttimejob.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
