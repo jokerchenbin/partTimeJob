f333481a9d2aae2036eefb02896fffe9ad594944
bin_chen <bin_chen@loongjoy.com>
2015-12-29 15:00:26 +0800

    登录注册到Bmob云端服务器

--- app/src/main/java/com/example/longjoy/parttimejob/activity/LoginActivity.java
+++ app/src/main/java/com/example/longjoy/parttimejob/activity/LoginActivity.java
@@ -1,24 +1,149 @@
 package com.example.longjoy.parttimejob.activity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Paint;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.longjoy.parttimejob.AppApplication;
 import com.example.longjoy.parttimejob.R;
+import com.example.longjoy.parttimejob.bean.UserInfo;
+import com.example.longjoy.parttimejob.common.FunctionUtils;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.List;
+
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.listener.FindListener;
+
+/**
+ * Created by 陈彬 on 2015/12/29  14:31
+ * 方法描述: 登录界面
+ */
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "LoginActivity";
+    private static final int RESULT_ERROR = 0;
+    private static final int RESULT_OK = 1;
 
     private TextView forgetPass;
+    private Intent intent;
+    private EditText et_username,et_password;
+    private Button btn_login,btn_registe;
+    private String telephone,password;
+    private Activity activity;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        activity = this;
+        context = this;
+        ((AppApplication) getApplication()).addActivity(activity);
         initViewID();
     }
 
     private void initViewID() {
         forgetPass = (TextView)findViewById(R.id.activity_login_tv_forgetPass);
         forgetPass.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+        et_username = (EditText) findViewById(R.id.activity_login_et_username);
+        et_password = (EditText) findViewById(R.id.activity_login_et_password);
+        btn_login = (Button) findViewById(R.id.activity_login_btn_login);
+        btn_login.setOnClickListener(this);
+        btn_registe = (Button) findViewById(R.id.activity_login_btn_registe);
+        btn_registe.setOnClickListener(this);
+        //得到相应的值
+        telephone = et_username.getText().toString().trim();
+        password = et_password.getText().toString().trim();
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.activity_login_btn_login:
+                //登录按钮
+                FunctionUtils.showLoadingDialog(activity);
+                loginToUserSystem();
+                break;
+            case R.id.activity_login_btn_registe:
+                //注册按钮
+                Intent intent = new Intent(activity,RegisterActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    /**
+     * Created by 陈彬 on 2015/12/29  14:30
+     * 方法描述: 登录到用户系统
+     */
+    private void loginToUserSystem() {
+        BmobQuery<UserInfo> query = new BmobQuery<>();
+        query.addWhereEqualTo("telephone", telephone);
+        query.findObjects(context, new FindListener<UserInfo>() {
+            @Override
+            public void onSuccess(List<UserInfo> list) {
+                Log.v(TAG,list.size()+"长度");
+
+                for (UserInfo userInfo:list){
+                    Log.v(TAG,userInfo.toString());
+                    if (userInfo.getPassword().equals(password)){
+                        //查询到相应的数据  返回登录成功
+                        handler.sendEmptyMessage(RESULT_OK);
+                        return;
+                    }
+                }
+                //没有找到相应的数据
+                handler.sendEmptyMessage(RESULT_ERROR);
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.v(TAG, i + "  --> " + s);
+                handler.sendEmptyMessage(RESULT_OK);
+            }
+        });
+    }
+
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+                switch (msg.what){
+                    case RESULT_OK:
+                        Toast.makeText(context,"登录成功",Toast.LENGTH_SHORT).show();
+                        FunctionUtils.dissmisLoadingDialog();
+                        break;
+                    case RESULT_ERROR:
+                        Toast.makeText(context,"登录失败",Toast.LENGTH_SHORT).show();
+                        FunctionUtils.dissmisLoadingDialog();
+                        break;
+                }
+        }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/longjoy/parttimejob/activity/RegisterActivity.java
+++ app/src/main/java/com/example/longjoy/parttimejob/activity/RegisterActivity.java
@@ -1,9 +1,13 @@
 package com.example.longjoy.parttimejob.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,26 +16,41 @@ import android.widget.Toast;
 import com.example.longjoy.parttimejob.AppApplication;
 import com.example.longjoy.parttimejob.R;
 import com.example.longjoy.parttimejob.bean.UserInfo;
+import com.example.longjoy.parttimejob.common.FunctionUtils;
+import com.example.longjoy.parttimejob.widget.AppAlertDialog;
 
+import org.json.JSONArray;
+
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.listener.FindCallback;
+import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.SaveListener;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "RegisterActivity";
+    private static final int RESULT_ERROR = 0;
+    private static final int RESULT_OK = 1;
     private EditText et_telephone, et_username, et_password, et_nextpassword;
     private String telephone, username, password, nextpassword;
     private Button btn_Register;
     private Activity activity;
     private String errMessage;
-
+    private Context context;
     private UserInfo userInfo;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        context = this;
         //加入Activity 中的 list 中
         activity = this;
         ((AppApplication) getApplication()).addActivity(activity);
@@ -54,7 +73,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.activity_register_btn_ok://注册按钮执行相应动作
-                registerUser("");
+                registerUser();
                 break;
         }
     }
@@ -64,7 +83,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
      * Created by 陈彬 on 2015/12/25  14:29
      * 方法描述: 注册
      */
-    private void registerUser(String name) {
+    private void registerUser() {
         /* 得到相应edittext中的值 */
         telephone = et_telephone.getText().toString().trim();
         username = et_username.getText().toString().trim();
@@ -81,16 +100,76 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             return;
         }
         Toast.makeText(activity, "正在注册，请稍后...", Toast.LENGTH_SHORT).show();
+        FunctionUtils.showLoadingDialog(activity);
         //调用注册的方法
         userInfo = new UserInfo();
         userInfo.setTelephone(telephone);
         userInfo.setUsername(username);
         userInfo.setPassword(password);
+        //注册用户前  检查一下数据库是否有相同的手机号码注册过
+        checkUserIsExits(telephone);
+    }
+
+    /**
+     * Created by 陈彬 on 2015/12/29  11:45
+     * 方法描述: 检查用户是否存在
+     */
+    private void checkUserIsExits(final String telephone) {
+        BmobQuery<UserInfo> query = new BmobQuery<>();
+        query.addWhereEqualTo("telephone", telephone);
+        query.setLimit(50);
+        query.findObjects(context, new FindListener<UserInfo>() {
+            @Override
+            public void onSuccess(List<UserInfo> list) {
+                Log.v(TAG, list.toString());
+                for (UserInfo userInfo:list){
+                    if (userInfo.getTelephone().equals(telephone)){
+                        handler.sendEmptyMessage(RESULT_ERROR);
+                        return;
+                    }
+                }
+                handler.sendEmptyMessage(RESULT_OK);
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.v(TAG, i + "" + s);
+                handler.sendEmptyMessage(RESULT_OK);
+            }
+        });
+    }
+
+
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case RESULT_OK:
+                    //保存用户信息到云端服务器
+                    saveUserToBmob();
+                    break;
+                case RESULT_ERROR:
+                    FunctionUtils.dissmisLoadingDialog();
+                    //查询到数据库中有相同的电话号码
+                    Toast.makeText(context,"该电话号码已注册",Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
+
+   /**
+    * Created by 陈彬 on 2015/12/29  14:15
+    * 方法描述: 保存用户信息到Bmob云端服务器上
+    */
+    private void saveUserToBmob(){
         userInfo.save(activity, new SaveListener() {
             @Override
             public void onSuccess() {
                 Toast.makeText(activity, "恭喜您，注册成功", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(activity,LoginActivity.class);
+                intent.putExtra("username",username);
                 startActivity(intent);
             }
 
@@ -99,7 +178,10 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 Toast.makeText(activity, "注册失败", Toast.LENGTH_SHORT).show();
             }
         });
+        FunctionUtils.dissmisLoadingDialog();
     }
+
+
 
     /**
      * 校验电话号码
