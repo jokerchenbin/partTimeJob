49b45a73a222ee59ea7388b969906e852edfd7a1
bin_chen <bin_chen@loongjoy.com>
2016-03-24 14:32:08 +0800

    添加头部滚动图片

--- app/src/main/java/com/example/longjoy/parttimejob/fragment/HomePageFragment.java
+++ app/src/main/java/com/example/longjoy/parttimejob/fragment/HomePageFragment.java
@@ -12,15 +12,24 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.example.longjoy.parttimejob.R;
 import com.example.longjoy.parttimejob.activity.JobDetailActivity;
+import com.example.longjoy.parttimejob.activity.MainActivity;
 import com.example.longjoy.parttimejob.adapter.ImageAdapter;
 import com.example.longjoy.parttimejob.widget.AutoScrollViewPager;
 import com.example.longjoy.parttimejob.adapter.JobInfoAdapter;
 import com.example.longjoy.parttimejob.bean.JobInfo;
 import com.example.longjoy.parttimejob.tools.ToastDiy;
 
+import java.util.HashMap;
 import java.util.List;
 
 import cn.bmob.v3.BmobQuery;
@@ -30,7 +39,7 @@ import cn.bmob.v3.listener.FindListener;
  * Created by 陈彬 on 2015/12/30  16:40
  * 方法描述:  首页
  */
-public class HomePageFragment extends Fragment {
+public class HomePageFragment extends Fragment implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener{
 
     private Button btn_add;
     private ListView list;
@@ -40,6 +49,7 @@ public class HomePageFragment extends Fragment {
     private AutoScrollViewPager viewPager;
     private ImageAdapter adapter;
     private Drawable[] drawables;
+    private SliderLayout mSlider;
 
     @Nullable
     @Override
@@ -49,7 +59,44 @@ public class HomePageFragment extends Fragment {
             initView(view);
             getData(getContext());
         }
+        showViewPager();
         return view;
+    }
+
+    private void showViewPager() {
+        HashMap<String,String> url_maps = new HashMap<String, String>();
+        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+
+        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
+        file_maps.put("Hannibal",R.mipmap.hannibal);
+        file_maps.put("Big Bang Theory",R.mipmap.bigbang);
+        file_maps.put("House of Cards",R.mipmap.house);
+        file_maps.put("Game of Thrones", R.mipmap.game_of_thrones);
+
+        for(String name : file_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(getContext());
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(file_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+
+            mSlider.addSlider(textSliderView);
+        }
+        mSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        mSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mSlider.setCustomAnimation(new DescriptionAnimation());
+        mSlider.setDuration(4000);
+        mSlider.addOnPageChangeListener(this);
     }
 
     /**
@@ -79,10 +126,7 @@ public class HomePageFragment extends Fragment {
         drawables = new Drawable[]{able, getResources().getDrawable(R.mipmap.wodeneiyebeijing), able, getResources().getDrawable(R.mipmap.wodeneiyebeijing)};
         list = (ListView) view.findViewById(R.id.firstpage_list);
         View v = View.inflate(getContext(), R.layout.head, null);
-        viewPager = (AutoScrollViewPager) v.findViewById(R.id.view_pager);
-        adapter = new ImageAdapter(getContext(), drawables);
-        viewPager.setAdapter(adapter);
-        viewPager.startAutoScroll();
+        mSlider = (SliderLayout) v.findViewById(R.id.slider);
         list.addHeaderView(v);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -96,4 +140,23 @@ public class HomePageFragment extends Fragment {
     }
 
 
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
+    }
 }
